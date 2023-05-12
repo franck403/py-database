203ca897310762ba689440b96d1057f249cf88a3
franck403 <79372051+franck403@users.noreply.github.com>
2023-05-12 13:59:56 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='py-database',
+      version='0.1',
+      description='py-database with api',
+      long_description='py-database with api for web',
+      author='Geoloup Team',
+      author_email='franckiebbb@gmail.com',
+      packages=['py-database'],
+)
